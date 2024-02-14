c896b9a3a3e89b8950e2fd92d6294eef61638987
Mihai Verzan <rtyh12@gmail.com>
2024-02-14 22:52:16 +0200

    Initial commit

---
+++ client/multiplayer.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+
+var IP_ADDRESS := "localhost"
+var PORT = 3000
+
+
+var black := false:
+	set(new_black):
+		black = new_black
+		modulate = Color.BLACK if new_black else Color.WHITE
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("click"):
+		black = not black
+		change_color_request.rpc(black)
+
+
+func _ready() -> void:
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_client(IP_ADDRESS, PORT)
+	multiplayer.multiplayer_peer = peer
+
+
+@rpc
+func change_color_request(new_black: bool):
+	print("i am client")
+
+
+@rpc
+func change_color_response(new_black: bool):
+	print("change_color_response")
+	black = new_black
---
+++ server/multiplayer.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+
+var IP_ADDRESS := "localhost"
+var PORT = 3000
+var MAX_CLIENTS = 10
+
+
+var black := false:
+	set(new_black):
+		black = new_black
+		modulate = Color.BLACK if new_black else Color.WHITE
+
+
+func _ready() -> void:
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_server(PORT, MAX_CLIENTS)
+	multiplayer.multiplayer_peer = peer
+
+	multiplayer.peer_connected.connect(peer_connected.bind())
+	
+
+@rpc("any_peer")
+func change_color_request(new_black: bool):
+	change_color_response.rpc(new_black)
+
+
+@rpc("any_peer")
+func change_color_response(new_black: bool):
+	print("i am server")
+
+
+func peer_connected(id: int):
+	print("penis ", id)
