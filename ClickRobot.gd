87bbebb30c06376116d24f21dbd7a6c678e53be3
Mihai Verzan <rtyh12@gmail.com>
2024-02-23 12:36:45 +0200

    Walk around multiplayer

--- ClickRobot.gd
+++ ClickRobot.gd
@@ -1,16 +1,26 @@
-extends TextureRect
+extends Node2D
 
 
-var IP_ADDRESS := "fairydust.cc"
-# var IP_ADDRESS := "159.89.20.135"
-var PORT = 3000
+var speed = 500
 
 @export var is_on := true:
 	set(new):
 		is_on = new
 		modulate = Color.WHITE if is_on else Color.BLACK
 
+func _enter_tree():
+	var peer_id_str = name.split("_")[1]
+	set_multiplayer_authority(int(peer_id_str))
 
-func _on_TextureRect_gui_input(event):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+func _physics_process(delta):
+	if not is_multiplayer_authority():
+		return
+	
+	if Input.is_action_pressed("space"):
 		is_on = not is_on
+	
+	var vec = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	position += vec * speed * delta
+
+	# if OS.get_cmdline_args().has("server"):
+	# 	print(position)
--- SetupMultiplayer.gd
+++ SetupMultiplayer.gd
@@ -1,14 +1,18 @@
 extends Node
 
 
-var peer := ENetMultiplayerPeer.new()
+@export var spawn_path: Node
+@export var robot_scene: PackedScene
 
-var IP_ADDRESS := "fairydust.cc"
-# var IP_ADDRESS := "172.22.226.71"
+var peer := ENetMultiplayerPeer.new()
+var is_server := OS.get_cmdline_args().has("server")
+
+# var IP_ADDRESS := "fairydust.cc"
+var IP_ADDRESS := "172.22.226.71"
 var PORT := 3000
 
 func _ready() -> void:
-	if OS.get_cmdline_args().has("server"):
+	if is_server:
 		peer.create_server(PORT)
 		print("server")
 	else:
@@ -19,4 +23,31 @@ func _ready() -> void:
 	peer.peer_connected.connect(peer_connected)
 
 func peer_connected(id: int):
-	print("penis ", id)
+	if is_server:
+		print("penis ", id)
+
+	c_request_join.rpc(peer.get_unique_id())
+
+@rpc("any_peer")
+func c_request_join(peer_id: int):
+	if not is_server:
+		return
+	print(peer_id, " requested join")
+	var robot = robot_scene.instantiate()
+	await get_tree().create_timer(1).timeout
+	robot.name = "player_" + str(peer_id)
+	spawn_path.add_child(robot, true)
+	s_request_join_response.rpc(true)
+
+@rpc("any_peer")
+func s_request_join_response(success: bool):
+	print("join response was: ", success)
+	if is_server:
+		return
+	if not success:
+		print("server refused join idk lol")
+		return
+	# var robot = robot_scene.instantiate()
+	# spawn_path.add_child(robot)
+	# robot.set_multiplayer_authority(peer.get_unique_id())
+	
